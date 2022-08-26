867266999d17644c5b04dd646c315e1191370731
adhamghazali <adham.ghazali@gmail.com>
2022-08-26 17:37:36 +0000

    function post and get api

--- spaceforge/monitor/views.py
+++ spaceforge/monitor/views.py
@@ -5,6 +5,7 @@ import pandas as pd
 from .apps import *
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import renderers
 
 
 
@@ -39,6 +40,16 @@ import base64
 from io import BytesIO
 
 
+class JPEGRenderer(renderers.BaseRenderer):
+    media_type = 'image/jpeg'
+    format = 'jpg'
+    charset = None
+    render_style = 'binary'
+
+    def render(self, data, media_type=None, renderer_context=None):
+        return data
+
+
 
 def encode_image(frame):
     
@@ -47,9 +58,104 @@ def encode_image(frame):
 
 class Prediction(APIView):
     
-      
          
     def post(self, request):
+        variation=False
+        #data = request.data
+        prompt= request.GET.get('prompt')
+        
+        print(prompt)
+        
+        
+        height = 512                        # default height
+        width = 512                         # default width 
+        batch_size = 1
+
+
+        num_inference_steps = 50            # Number of denoising steps
+        guidance_scale = 1                 # Scale for classifier-free guidance
+
+        generator = torch.manual_seed(random.randint(0,25000000))   # Seed generator to create the inital latent noise
+
+        scheduler = LMSDiscreteScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", num_train_timesteps=1000)
+
+        text_input = ApiConfig.tokenizer(prompt, padding="max_length", max_length=ApiConfig.tokenizer.model_max_length, truncation=True, return_tensors="pt")
+
+        with torch.no_grad():
+          text_embeddings = ApiConfig.text_encoder(text_input.input_ids.to(ApiConfig.torch_device0))[0]
+
+        max_length = text_input.input_ids.shape[-1]
+        uncond_input = ApiConfig.tokenizer(
+            [""] * batch_size, padding="max_length", max_length=max_length, return_tensors="pt"
+        )
+        with torch.no_grad():
+          uncond_embeddings = ApiConfig.text_encoder(uncond_input.input_ids.to(ApiConfig.torch_device0))[0]
+
+        text_embeddings = torch.cat([uncond_embeddings, text_embeddings])
+        
+        latents = torch.randn((batch_size, ApiConfig.unet.in_channels, height // 8, width // 8),
+                              generator=generator,
+                             )
+           
+                      
+        
+        latents = latents.to(ApiConfig.torch_device0)
+
+        scheduler.set_timesteps(num_inference_steps)
+        latents = latents * scheduler.sigmas[0]
+
+        with autocast("cuda"):
+
+          for i, t in tqdm(enumerate(scheduler.timesteps)):
+            # expand the latents if we are doing classifier-free guidance to avoid doing two forward passes.
+            latent_model_input = torch.cat([latents] * 2)
+            sigma = scheduler.sigmas[i]
+            latent_model_input = latent_model_input / ((sigma**2 + 1) ** 0.5)
+
+            # predict the noise residual
+            with torch.no_grad():
+              noise_pred = ApiConfig.unet(latent_model_input, t, encoder_hidden_states=text_embeddings)["sample"]
+
+            # perform guidance
+            noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+            noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
+            # compute the previous noisy sample x_t -> x_t-1
+            latents = scheduler.step(noise_pred, i, latents)["prev_sample"]
+
+        # scale and decode the image latents with vae
+        latents = 1 / 0.18215 * latents
+
+        with torch.no_grad():
+          image = ApiConfig.vae.decode(latents)
+                        
+                        
+        image = (image / 2 + 0.5).clamp(0, 1)
+        img=image.detach().cpu().permute(0, 2,3, 1).squeeze(0).numpy()*255
+        image = image.detach().cpu().permute(0, 2, 3, 1).numpy()
+
+        images = (image * 255).round().astype("uint8")
+        pil_images = [Image.fromarray(image) for image in images]
+        
+        output, _ = ApiConfig.upsampler.enhance(img, outscale=4)
+        out_image = Image.fromarray(output)
+        
+        
+        
+        img_buffer = BytesIO()
+        out_image.save(img_buffer, format='JPEG')
+        byte_data = img_buffer.getvalue()
+        encoded_image = base64.b64encode(byte_data)
+        
+        #image_data = base64.b64encode(encoded_image).decode('utf-8')
+        
+        #renderer=JPEGRenderer()
+    
+        
+        
+        return Response(encoded_image,content_type="image/png")
+    
+    def get(self, request):
+        
         variation=False
         #data = request.data
         prompt= request.GET.get('prompt')
@@ -120,24 +226,39 @@ class Prediction(APIView):
                         
                         
         image = (image / 2 + 0.5).clamp(0, 1)
-        #img=image.detach().cpu().permute(0, 2,3, 1).squeeze(0).numpy()*255
+        img=image.detach().cpu().permute(0, 2,3, 1).squeeze(0).numpy()*255
         image = image.detach().cpu().permute(0, 2, 3, 1).numpy()
 
         images = (image * 255).round().astype("uint8")
         pil_images = [Image.fromarray(image) for image in images]
         
+        output, _ = ApiConfig.upsampler.enhance(img, outscale=2)
+        out_image = Image.fromarray(output)
+        
+        
         
         img_buffer = BytesIO()
-        pil_images[0].save(img_buffer, format='JPEG')
+        out_image.save(img_buffer, format='png')
         byte_data = img_buffer.getvalue()
         encoded_image = base64.b64encode(byte_data)
-
-      
-        #encoded_image=base64.b64encode(pil_images[0])
+        
+        image_data = base64.b64encode(byte_data).decode('utf-8')
         
         
+        ctx={'image':image_data}
         
-        #response_dict = {"Output Image": encoded_image}
+        #ctx["image"] = image_data
         
         
-        return Response(encoded_image, status=200)
+    
+        
+            
+        
+        
+        return  render(request, 'index.html',ctx)
+        
+        
+    
+    
+    
+    
--- spaceforge/spaceforge/settings.py
+++ spaceforge/spaceforge/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'spaceforge.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/home/adham/repo/spaceforge-deploy/spaceforge/monitor/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import os
 MODELS ='../../../models/' #os.path.join(BASE_DIR, '../../../models')
+
+TEMPLATE_DIRS = (
+    os.path.join('../monitor/', 'templates'),
+)
